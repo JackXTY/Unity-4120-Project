7ab37c195cd27b8a39a09ce64b5f81ee4d7b38c6
JackXTY <jack123qd@hotmail.com>
2021-05-05 18:38:25 +0800

    update cannon

---
+++ Assets/Scripts/LevelDesign/Shell.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shell : MonoBehaviour
+{
+    public float aliveTime = 4f;
+    public int damage = 10;
+    private float counter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+        if(counter > aliveTime){
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider col){
+        if(col.tag == "Player"){
+            col.gameObject.GetComponent<UnityChanSimpleController>().damage(damage);
+        }
+        Destroy(this.gameObject);
+    }
+}
