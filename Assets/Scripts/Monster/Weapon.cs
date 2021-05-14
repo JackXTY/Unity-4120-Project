df7455b6a99160c73a34dd45d96d28e35ef36816
JackXTY <jack123qd@hotmail.com>
2021-05-14 14:34:07 +0800

    basic interaction between player and monster

---
+++ Assets/Scripts/Monster/Weapon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public int attack1 = 10;
+    public int attack2 = 20;
+    private int attackWay = 0;
+    // true - attack1 (small attack)
+    // false - attack2 (large attack)
+
+    public void changeAttack(int way){
+        attackWay = way;
+        Debug.Log("change " + way);
+    }
+
+    private void OnTriggerEnter(Collider other){
+        Debug.Log(other.name);
+        if(other.tag=="Monster" && attackWay!=0){
+            int attackValue = attack1;
+            if(attackWay == 2){
+                attackValue = attack2;
+            }
+            other.gameObject.GetComponent<MonsterController>().hit(attackValue);
+        }
+    }
+}
